5fa246c7c6f93d33c03c4c3fd5192362e718b19f
Jamel Travis <jameltravis@gmail.com>
2017-09-03 15:11:07 -0400

    js practice and website start 09021017

---
+++ javaScriptPractice/jsPractice.js
@@ -0,0 +1,20 @@
+// Array Prcatice
+var NHoods = ["Midtown", "Grenich Village", "Harlem", "Financial District"];
+
+// "Push" adds data to the end of an array
+NHoods.push("The Heights");
+
+// "Pop" removes the last item in an array
+NHoods.pop();
+
+// "Unshift" adds an item to the front of an array
+NHoods.unshift("Flatiron");
+
+// "Shift" removes the first element in an array
+NHoods.shift();
+
+// "indexOf" lets you know what the index is
+NHoods.indexOf("Flatiron");
+
+// "slice" copies parts of an array and takes two arguments:
+// where the slice should start and where it should end.
